561a50ad8371fc422de902c5ae37319dc7095014
oxygen77 <>
2007-10-11 20:44:09 +0000

    bugfix  in bmp loader for av3,av1 and jbmm
    git-svn-id: http://svn.code.sf.net/p/archopen/code@165 18109dc0-e51e-0410-b7de-bd5de724b689

--- ArchOpen/trunk/medios/gfx/bmp.c
+++ ArchOpen/trunk/medios/gfx/bmp.c
@@ -23,13 +23,15 @@
 #include <fs/stdfs.h>
 
 #include <sys_def/errors.h>
+#include <sys_def/stddef.h>
+
 
 #include <math.h>
 
 extern struct graphicsBuffer VIDEO_1;
-bmpColor * bmpPalette;
+static bmpColor * bmpPalette;
 
-unsigned int numberColor(int bitcount)
+unsigned int bmp_numberColor(int bitcount)
 {
     unsigned int res;
     switch(bitcount)
@@ -47,7 +49,7 @@ unsigned int numberColor(int bitcount)
 
 
 
-void read1BitRow(unsigned int * buffer,char * data,int width)
+void bmp_read1BitRow(unsigned int * buffer,char * data,int width)
 {
     int i,j,k;;
     bmpColor * tmp,*tmp2;
@@ -55,7 +57,7 @@ void read1BitRow(unsigned int * buffer,char * data,int width)
     char dataVal;
 
     printk("width=%d hasMore=%d w=%d\n",width,hasMore,width/8);
-    
+
     width/=8;
     j=0;
 
@@ -89,7 +91,7 @@ void read1BitRow(unsigned int * buffer,char * data,int width)
             j++;
             hasMore-=2;
         }
-        
+
         if(hasMore==1)
         {
             //printk("still to put\n");
@@ -101,18 +103,18 @@ void read1BitRow(unsigned int * buffer,char * data,int width)
     }
 }
 
-void read4BitRow(unsigned int * buffer,char * data,int width)
+void bmp_read4BitRow(unsigned int * buffer,char * data,int width)
 {
     int i;
     bmpColor * tmp,*tmp2;
-    
+
     int hasMore=width%2;
     char dataVal;
 
     i=0;
-    
+
     width/=2;
-    
+
     for(i=0;i<width;i++)
     {
         dataVal=data[i];
@@ -130,14 +132,14 @@ void read4BitRow(unsigned int * buffer,char * data,int width)
     }
 }
 
-void read8BitRow(unsigned int * buffer,char * data,int width)
+void bmp_read8BitRow(unsigned int * buffer,char * data,int width)
 {
     int i;
     bmpColor * tmp,*tmp2;
     int hasMore=width%2;
 
     width/=2;
-    
+
     for(i=0;i<width;i++)
     {
         tmp2=&bmpPalette[data[i*2]&0xFF];
@@ -154,25 +156,25 @@ void read8BitRow(unsigned int * buffer,char * data,int width)
     }
 }
 
-void read16BitRow(unsigned int * buffer,char * data,int width)
+void bmp_read16BitRow(unsigned int * buffer,char * data,int width)
 {
 }
 
-void read24BitRow(unsigned int * buffer,char * data,int width)
+void bmp_read24BitRow(unsigned int * buffer,char * data,int width)
 {
     int i,k;
     int r,g,b,Y1,Y2,Cr,Cb;
     int hasMore=width%2;
-    width/=2;    
+    width/=2;
     k=0;
-    
+
     for(i=0;i<width;i++)
     {
-    
+
         b=data[k++];
         g=data[k++];
         r=data[k++];
-        
+
         Y1=RGB2Y(r,g,b);
         Cr=RGB2Cr(r,g,b);
         Cb=RGB2Cb(r,g,b);
@@ -180,20 +182,20 @@ void read24BitRow(unsigned int * buffer,char * data,int width)
         b=data[k++];
         g=data[k++];
         r=data[k++];
-        
+
         Y2=RGB2Y(r,g,b);
         Cr=(Cr+(RGB2Cr(r,g,b)))>>1;
         Cb=(Cb+RGB2Cb(r,g,b))>>1;
-        
+
         buffer[i]=(unsigned int)((Y2&0xFF)<<24 | (Cr&0xFF) <<16 | (Y1&0xFF)<<8 | (Cb&0xFF));
     }
-    
+
     if(hasMore)
     {
         b=data[k++];
         g=data[k++];
         r=data[k++];
-        
+
         Y1=RGB2Y(r,g,b);
         Cr=RGB2Cr(r,g,b);
         Cb=RGB2Cb(r,g,b);
@@ -201,9 +203,114 @@ void read24BitRow(unsigned int * buffer,char * data,int width)
     }
 }
 
-void read32BitRow(unsigned int * buffer,char * data,int width)
+void bmp_read32BitRow(unsigned int * buffer,char * data,int width)
 {
     /* TODO: not supported atm */
+}
+
+#define BMP_RLE_DISCOVER 0
+#define BMP_RLE_ENCODED 1
+#define BMP_RLE_ABSOLUTE 2
+
+static int rle_mode;
+static int uses_rle;
+static int nbToRead;
+static char bmp_tmpBuff[0x100];
+static int bmpIsRead;
+static int posInfo;
+
+void bmp_initFileReader(int hasRle)
+{
+    if(hasRle)
+    {
+        uses_rle=1;
+        rle_mode=BMP_RLE_DISCOVER;
+    }
+    else
+    {
+        uses_rle=0;
+    }
+}
+
+int bmp_readFile(int fd,char* buffer,int nbBytes)
+{
+    char tag[2];
+    int bmp_rle_done=0;
+    int nbRead;
+    char charToDup;
+    int i;
+    char buff[0x100];
+
+    if(uses_rle)
+    {
+        while(!bmp_rle_done)
+        {
+            switch(rle_mode)
+            {
+                case BMP_RLE_DISCOVER:
+                    if(read(fd,tag,2)!=2)
+                    {
+                        printk("[BMP_RLE] error reading tag\n");
+                        return 0;
+                    }
+                    if(tag[0]==0)
+                    {
+                        switch(tag[1])
+                        {
+                            case 0x0: /*endOfLine*/
+                                printk("End of line\n");
+                                break;
+                            case 0x1: /*endOfBmp*/
+                                printk("End of bmp\n");
+                                break;
+                            case 0x2: /*Delta*/
+                                printk("Delta in img\n");
+                                break;
+                            default: /*absolute mode*/
+                                nbToRead=tag[1];
+                                rle_mode=BMP_RLE_ABSOLUTE;
+                                bmpIsRead=0;
+                                posInfo=0;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        rle_mode=BMP_RLE_ENCODED;
+                        nbToRead=tag[0];
+                        charToDup=tag[1];
+                    }
+                    break;
+
+                case BMP_RLE_ENCODED: /* attention ne marche pas car il faut diminuer nbBytes*/
+                    for(i=0;i<nbBytes && nbToRead>0;i++,nbToRead--)
+                        buffer[i]=charToDup;
+                    if(nbToRead==0)
+                        rle_mode=BMP_RLE_DISCOVER;
+                    break;
+
+                case BMP_RLE_ABSOLUTE:
+                    if(!bmpIsRead)
+                    {
+                        if(read(fd,bmp_tmpBuff,nbToRead)!=nbToRead)
+                        {
+                            printk("Error reading from file\n");
+                            return 0;
+                        }
+                        bmpIsRead=1;
+                    }
+                    for(i;nbBytes>0 && nbToRead>0;nbBytes--,nbToRead--)
+                    {
+                        
+                    }
+                    break;
+            }
+        }
+    }
+    else
+    {
+        return read(fd,buffer,nbBytes);
+    }
 }
 
 MED_RET_T gfx_loadBmp(char * filename)
@@ -226,7 +333,7 @@ MED_RET_T gfx_loadBmp(char * filename)
 
     /*reading VID1 plane info*/
     gfx_planeGetSize(VID1,&scr_w,&scr_h,NULL);
-    
+
     if ((fd = open(filename, O_RDONLY)) < 0 )
     {
         printk("[gfx_loadBmp]: can't open %s\n", filename);
@@ -246,21 +353,21 @@ MED_RET_T gfx_loadBmp(char * filename)
         close(fd);
         return -MED_ERROR;
     }
-    
+
     printk("File: %s\n",filename);
     fileHeader.type=(*tmp_int)&0xFFFF;
     fileHeader.size=((tmp_int[0]>>16)&0xFFFF)|(tmp_int[1]&0xFFFF);
     fileHeader.reserved1=(tmp_int[1]>>16)&0xFFFF;
     fileHeader.reserved2=tmp_int[2]&0xFFFF;
     fileHeader.offsetbits=((tmp_int[2]>>16)&0xFFFF)|(tmp_int[3]&0xFFFF);
-    
+
     printk("File Header:\nType=%04x, Size=%08x, Res1=%04x, Res2=%04x, offsetbits=%08x\n",
     fileHeader.type,
     fileHeader.size,
     fileHeader.reserved1,
     fileHeader.reserved2,
     fileHeader.offsetbits);
-    
+
     printk("Info Header:\nSize=%x, width=%x, height=%x, planes=%x, bitcount=%x\n"
             "SizeImage=%x, xpelspermeter=%x, ypelspermeter=%x\n"
             "colorsused=%x, colorsimportant=%x\n",
@@ -291,7 +398,7 @@ MED_RET_T gfx_loadBmp(char * filename)
         printk("[BMP ERROR] unsupported format for %s (compression=%d)\n",filename,infoHeader.compression);
         close(fd);
         return -MED_ERROR;
-    }   
+    }
 
     /*bit field and 16bit file ?*/
     if(infoHeader.compression == 3 && infoHeader.bitcount != 16)
@@ -330,7 +437,7 @@ MED_RET_T gfx_loadBmp(char * filename)
     {
         /*palette size determined using space btw header and img data*/
         int paletteSize=fileHeader.offsetbits-54;
-        char * palette_buf; 
+        char * palette_buf;
         int r,g,b,res;
         palette_buf = (char*)malloc(paletteSize);
         if(!palette_buf)
@@ -339,7 +446,7 @@ MED_RET_T gfx_loadBmp(char * filename)
             close(fd);
             return -MED_ERROR;
         }
-        
+
         /*reading palette*/
         if(read(fd,palette_buf,paletteSize)!=paletteSize)
         {
@@ -349,18 +456,18 @@ MED_RET_T gfx_loadBmp(char * filename)
         }
 
         /*creating palette*/
-        bmpPalette=(bmpColor*)malloc(numberColor(infoHeader.bitcount)*sizeof(bmpColor));
+        bmpPalette=(bmpColor*)malloc(bmp_numberColor(infoHeader.bitcount)*sizeof(bmpColor));
         if(!bmpPalette)
         {
             printk("[BMP ERROR] error doing malloc for palette (size=%d)\n",
-                   numberColor(infoHeader.bitcount)*sizeof(bmpColor));
+                   bmp_numberColor(infoHeader.bitcount)*sizeof(bmpColor));
             close(fd);
             free(palette_buf);
             return -MED_ERROR;
         }
 
-        printk("Reading palette: %d colors / %d\n",paletteSize,numberColor(infoHeader.bitcount));
-        
+        printk("Reading palette: %d colors / %d\n",paletteSize,bmp_numberColor(infoHeader.bitcount));
+
         /* init palette with read data*/
         for(i=0,j=0;i<paletteSize;i+=4,j++)
         {
@@ -375,7 +482,7 @@ MED_RET_T gfx_loadBmp(char * filename)
                    bmpPalette[i].Y,bmpPalette[i].Cr,bmpPalette[i].Cb);
         }
         /*init rest of palette with white*/
-        for(i=j;i<numberColor(infoHeader.bitcount);i++)
+        for(i=j;i<bmp_numberColor(infoHeader.bitcount);i++)
             bmpPalette[i].Y=bmpPalette[i].Cr=bmpPalette[i].Cb=0;
 
         free(palette_buf);
@@ -394,8 +501,8 @@ MED_RET_T gfx_loadBmp(char * filename)
     rowSize=infoHeader.width*infoHeader.bitcount/8;
     while(8*rowSize<infoHeader.width*infoHeader.bitcount) rowSize++;
     while(rowSize%4) rowSize++;
-    
-    
+
+
     tmpRow=(char *)malloc(rowSize);
     if(!tmpRow)
     {
@@ -404,23 +511,23 @@ MED_RET_T gfx_loadBmp(char * filename)
         if(bmpPalette) free(bmpPalette);
         return -MED_ERROR;
     }
-     
+
     /* reading data to screen */
     screenDirect=(unsigned int *)gfx_planeGetBufferOffset(VID1);
-    
+
     for (j=0;j<scr_h;j++)
         for (i=0;i<scr_w;i++)
             screenDirect[j*scr_w + i] = 0x00800080;
-    
+
     img_x=(SCREEN_WIDTH-infoHeader.width)/2;
     img_y=(scr_h-infoHeader.height)/2;
-    
+
     printk("[BMP info]: output x=%d, y=%d, w=%d, h=%d (screen: %d-%d,%d)\n",img_x,img_y,
            infoHeader.width,infoHeader.height,scr_w,SCREEN_WIDTH,scr_h),
-    
+
     //buff_offset=&screenDirect[img_x/2+(img_y+infoHeader.height-1)*scr_w];
     buff_offset=screenDirect+infoHeader.height*scr_w;
-    
+
     if(infoHeader.bitcount==16)
     {
         /*NOTE: need code for 16bit bmp */
@@ -440,21 +547,21 @@ MED_RET_T gfx_loadBmp(char * filename)
                 free(tmpRow);
                 return -MED_ERROR;
             }
-            
+
             /* converting 1 row */
             switch(bitDepth)
             {
-                case 1 : read1BitRow(buff_offset,tmpRow,width); break;
-                case 4 : read4BitRow(buff_offset,tmpRow,width); break;
-                case 8 : read8BitRow(buff_offset,tmpRow,width); break;
-                case 24: read24BitRow(buff_offset,tmpRow,width); break;
-                case 32: read32BitRow(buff_offset,tmpRow,width); break;
+                case 1 : bmp_read1BitRow(buff_offset,tmpRow,width); break;
+                case 4 : bmp_read4BitRow(buff_offset,tmpRow,width); break;
+                case 8 : bmp_read8BitRow(buff_offset,tmpRow,width); break;
+                case 24: bmp_read24BitRow(buff_offset,tmpRow,width); break;
+                case 32: bmp_read32BitRow(buff_offset,tmpRow,width); break;
             }
             /* moving to next row in img */
             buff_offset-=scr_w;
         }
     }
-    
+
     close(fd);
     printk("bf free1\n");
     if(bmpPalette) free(bmpPalette);
