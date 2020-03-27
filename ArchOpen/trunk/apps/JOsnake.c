59435344c6b49148ddb70e65f4f85bdc7b1de94e
joel <joel.martin.2104@gmail.com>
2020-03-27 21:27:38 +0100

    tests JOsnake

---
+++ ArchOpen/trunk/apps/JOsnake.c
@@ -0,0 +1,8 @@
+//
+//  JOsnake.c
+//  
+//
+//  Created by joel on 27/03/2020.
+//
+
+#include <stdio.h>
---
+++ ArchOpen/trunk/sine.c
@@ -0,0 +1,8 @@
+//
+//  sine.c
+//  
+//
+//  Created by joel on 27/03/2020.
+//
+
+#include <stdio.h>
